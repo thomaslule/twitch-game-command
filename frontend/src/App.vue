aa807fe7ec42bebc228f784f698f4d486ea4714b
thomaslule <thomas@lule.fr>
2020-05-16 15:38:48 +0200

    Init frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Descriptions />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Descriptions from "./components/Descriptions.vue";
 
 @Component({
   components: {
-    HelloWorld
+    Descriptions
   }
 })
 export default class App extends Vue {}
@@ -22,8 +21,5 @@ export default class App extends Vue {}
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ frontend/src/components/Descriptions.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-if="descriptions">
+    <button v-on:click="update">Update</button>
+    <ul>
+      <li v-for="description in descriptions" :key="description.game">
+        <input v-model="description.game" />
+        <br />
+        <textarea v-model="description.text" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Descriptions extends Vue {
+  public descriptions = [{ game: "Game 1", text: "description 1" }];
+
+  public mounted() {
+    fetch("http://localhost:3000/descriptions")
+      .then(res => res.json())
+      .then(descriptions => {
+        this.descriptions = descriptions;
+      })
+      .catch(err => console.error(err));
+  }
+
+  public update() {
+    console.log(this.descriptions[0].game, this.descriptions[0].text);
+  }
+}
+</script>
