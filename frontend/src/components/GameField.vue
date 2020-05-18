a78ee68e3ffd9fb3a746a26f0c12425d1431d77f
thomaslule <thomas@lule.fr>
2020-05-18 19:51:53 +0200

    Add keyboard support

--- frontend/src/components/GameField.vue
+++ frontend/src/components/GameField.vue
@@ -5,6 +5,9 @@
       v-on:input="updateValue($event.target.value)"
       required
       v-bind:placeholder="$t('configForm.game')"
+      @keydown.down.prevent="onArrowDown"
+      @keydown.up.prevent="onArrowUp"
+      @keydown.enter.prevent="onEnter"
     />
     <ul class="autocomplete-results" v-show="autocompleteVisible">
       <li
@@ -12,6 +15,8 @@
         :key="i"
         @click="updateValue(result, true)"
         class="autocomplete-result"
+        :class="{ active: i === arrowCounter }"
+        @mouseover="onMouseOver(i)"
       >
         {{ result }}
       </li>
@@ -30,6 +35,7 @@ export default class GameField extends Vue {
   public clientId?: string;
   public autocompleteList: string[] = [];
   public autocompleteVisible = false;
+  public arrowCounter = -1;
 
   public async mounted() {
     document.addEventListener("click", this.handleClickOutside);
@@ -54,6 +60,30 @@ export default class GameField extends Vue {
       this.autocompleteList = await this.fetchAutocompleteList(value);
       this.autocompleteVisible = true;
     }
+  }
+
+  public onArrowDown() {
+    if (this.arrowCounter < this.autocompleteList.length - 1) {
+      this.arrowCounter = this.arrowCounter + 1;
+    }
+  }
+
+  public onArrowUp() {
+    if (this.arrowCounter >= 0) {
+      this.arrowCounter = this.arrowCounter - 1;
+    }
+  }
+
+  public async onEnter() {
+    if (this.arrowCounter >= 0) {
+      await this.updateValue(this.autocompleteList[this.arrowCounter], true);
+      this.autocompleteVisible = false;
+      this.arrowCounter = -1;
+    }
+  }
+
+  public onMouseOver(index) {
+    this.arrowCounter = index;
   }
 
   private async fetchAutocompleteList(value: string) {
@@ -105,7 +135,7 @@ export default class GameField extends Vue {
   cursor: pointer;
 }
 
-.autocomplete-result:hover {
+.autocomplete-result.active {
   background-color: #4aae9b;
   color: white;
 }
